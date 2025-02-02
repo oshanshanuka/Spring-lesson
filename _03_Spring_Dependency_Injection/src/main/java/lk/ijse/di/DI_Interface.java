3c01e214352257d53fe44d94f33c93963d9739c4
oshan shanuka <oshanshanuka428@gmail.com>
2025-02-02 20:21:55 +0530

    Fix an error

--- _03_Spring_Dependency_Injection/src/main/java/lk/ijse/di/DI_Interface.java
+++ _03_Spring_Dependency_Injection/src/main/java/lk/ijse/di/DI_Interface.java
@@ -1,6 +1,11 @@
 package lk.ijse.di;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public interface DI_Interface {
     void inject(Test1 test1);
+
+    @Autowired
+    void inject(DI test1);
 }
--- _03_Spring_Dependency_Injection/src/main/java/lk/ijse/di/Test2.java
+++ _03_Spring_Dependency_Injection/src/main/java/lk/ijse/di/Test2.java
@@ -5,30 +5,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Test2 implements DI_Interface{
-
     //@Autowired
     DI test1;
 
-    public Test2(){
-        System.out.println("Test2 constrctor");
-    }
-
-    /*public Test2(DI test1) {
+    /*@Autowired
+    public Test2(DI test1) {
+       this.test1 = test1;
+    }*/
+   /* @Autowired
+    public void setter(DI test1) {
         this.test1 = test1;
     }*/
-
-    @Autowired
-    public void setter(DI test1){
-        this.test1 = test1;
-    }
-
-    public void display(){
+    public void display() {
         test1.sayHello();
     }
 
     @Override
+    public void inject(Test1 test1) {
+
+    }
+
+    @Autowired
+    @Override
     public void inject(DI test1) {
         this.test1 = test1;
     }
+
 
 }
